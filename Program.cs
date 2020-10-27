231213b337e79d1277ab43a4801d0cdd9223fa02
Juan Carlos Pasillas <37219813+JCPasillas@users.noreply.github.com>
2020-10-27 14:28:34 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CECS342_Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
